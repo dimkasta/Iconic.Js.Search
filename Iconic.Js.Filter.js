e9aacd45e9971158e88ec0edbce14080a86c8df8
dimitris <dimkasta@yahoo.gr>
2018-05-11 03:05:27 +0300

    meh

--- Iconic.Js.Filter.js
+++ Iconic.Js.Filter.js
@@ -1,7 +1,24 @@
-function FilterService(element) {
-    var activeClass = "active";
+function FilterService(configuration) {
+    //Initialization
+    if (configuration === null || configuration === undefined) {
+        configuration = {};
+    }
+
+    if (!configuration.hasOwnProperty("activeClass")) {
+        configuration.activeClass = "active";
+    }
+
+
+    console.log(configuration);
     //TODO: use constants for types etc.
     //TODO: get type, name, value based on the type of the element
     //TODO: add listeners etc
-    return {};
+    return {
+        createFromSelector: function (elementSelector) {
+
+        },
+        createFromElement: function (element) {
+
+        }
+    };
 }
